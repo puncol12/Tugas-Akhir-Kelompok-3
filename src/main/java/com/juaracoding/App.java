35468d3ec587c0a413480585b572c818b0542321
[yusuf nur rizqi] <yn.yusufnur@gmail.com>
2025-06-18 21:45:11 +0700

    wip

--- src/main/java/com/juaracoding/App.java
+++ src/main/java/com/juaracoding/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! 1234567" );
+        System.out.println( "Hello World! 1234567sadasdasdasdasdasdasdasdasdasd" );
     }
 }
