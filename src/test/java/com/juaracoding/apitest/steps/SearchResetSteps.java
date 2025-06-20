bdf425cf9bf0e030640f224e512dc1d53e104a74
[yusuf nur rizqi] <yn.yusufnur@gmail.com>
2025-06-20 21:43:45 +0700

    WIP submenu unit (previous page)

---
+++ src/test/java/com/juaracoding/apitest/steps/SearchResetSteps.java
@@ -0,0 +1,33 @@
+package com.juaracoding.apitest.steps;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+
+import com.juaracoding.apitest.DriverSingleton;
+import com.juaracoding.apitest.pages.UnitPage;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+public class SearchResetSteps {
+
+    WebDriver driver;
+    UnitPage unitPage;
+
+    @Given("Klik tombol Reset")
+    public void klikTombolReset() {
+        driver = DriverSingleton.createOrGetDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        unitPage = UnitPage.init(driver);
+        unitPage.resetButton();
+    }
+
+    @Then("Input pada kotak Search dihapus dan data unit kembali ke kondisi awal")
+    public void resetData() {
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        boolean isExsist = unitPage.isDataRestored();
+        Assert.assertTrue(isExsist, "Data unit tidak kembali ke kondisi awal setelah reset");
+    }
+}
